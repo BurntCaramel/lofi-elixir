d17d870e9b0f924d7212079319d17cb66a693e8e
Patrick Smith <patrick@burntcaramel.com>
2017-08-25 19:35:20 +1000

    Improve docs

--- lib/lofi_resolve.ex
+++ lib/lofi_resolve.ex
@@ -4,7 +4,7 @@ defmodule Lofi.Resolve do
   """
 
   @doc """
-  Resolve string from `texts` and `mentions`, substituting values for any @mentions found by calling `get_mention_value` with the mention key
+  Resolve string from `texts` and `mentions`, substituting values for any @mentions found by calling `get_mention_value` with the mention key.
   """
   def resolve_content(texts, mentions, get_mention_value) when is_function(get_mention_value) do
     [texts, mentions]
@@ -14,14 +14,14 @@ defmodule Lofi.Resolve do
   end
 
   @doc """
-  Resolve string from `texts` and `mentions`, substituting values for any @mentions found by looking up value in map `values` for the mention key, defaulting to `fallback_value` if mention key not present
+  Resolve string from `texts` and `mentions`, substituting values for any @mentions found by looking up value in map `values` for the mention key. If key is not present, then `fallback_value` is used.
   """
   def resolve_content(texts, mentions, values, fallback_value) when is_map(values) do
     resolve_content(texts, mentions, fn(key) -> Map.get(values, key, fallback_value) end)
   end
 
   @doc """
-  Resolve string from `texts` and `mentions`, substituting values for any @mentions found by looking up value in map `values` for the mention key, raising `KeyError` if mention key not present
+  Resolve string from `texts` and `mentions`, substituting values for any @mentions found by looking up value in map `values` for the mention key. . If key is not present, a `KeyError` exception is raised.
   """
   def resolve_content!(texts, mentions, values) when is_map(values) do
     resolve_content(texts, mentions, fn(key) -> Map.fetch!(values, key) end)
